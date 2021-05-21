013de09329158444b4d9ead5b9582c5ee00d5002
Bryan <bryanmuliawan@yahoo.co.id>
2021-05-21 22:03:05 +0700

    Update addStockItem.php
    Remove unused code

--- addStockItem.php
+++ addStockItem.php
@@ -30,20 +30,6 @@ require_once("stockItem_controller.php");
 
         createImageData($judul, $deskripsi, $gambar_data, $harga);
     }
-
-    // // This is the file we're going to add it in the database
-    // $MY_FILE = $_FILES['file']['tmp_name'];
-
-    // // To open the file and store its contents in $file_contents
-    // $file = fopen($MY_FILE, 'r');
-    // $file_contents = fread($file, filesize($MY_FILE));
-    // fclose($file);
-    // /* We need to escape some stcharacters that might appear in  file_contents,so do that now, before we begin the query.*/
-
-    // $file_contents = addslashes($file_contents);
-
-    // // To add the file in the database
-    // // mysql_query("INSERT INTO files SET file_data='$file_contents'")
     ?>
 </body>
 
