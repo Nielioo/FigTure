e72eabdd98d54676691af900aaca6f9279c42ccb
Bryan <bryanmuliawan@yahoo.co.id>
2021-05-21 22:03:45 +0700

    Update stockItem_controller.php
    Fix image uploading issue

--- stockItem_controller.php
+++ stockItem_controller.php
@@ -16,17 +16,20 @@ function createImageData($judul, $deskripsi, $gambar_data, $harga) {
             // $image_size = $_FILES['gambar']['size'];
             // $image_error = $_FILES['gambar']['error'];
 
-
             // Add images to database
+            // Directly
+            // $query = "INSERT INTO `image_data`(`judul`, `deskripsi`, `gambar`, `harga`) VALUES ('$judul', '$deskripsi', '$gambar_data', '$harga')";
+            // $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+
+            // Prepare
             $query = $connection->prepare("INSERT INTO `image_data`(`judul`, `deskripsi`, `gambar`, `harga`) VALUES (?, ?, ?, ?)");
             
-            if ($query) {
-                $query->bind_param("ssbi", $judul, $deskripsi, $gambar_data, $harga);
-                $query->execute() or die(mysqli_error($connection));
-                $result = $query->get_result();
-            } else {
-                echo "error";
-            }
+            $null = NULL;
+            $query->bind_param("ssbi", $judul, $deskripsi, $null, $harga);
+            $query->send_long_data(2, $gambar_data);
+            $query->execute() or die(mysqli_error($connection));
+
+            // $result = $query->get_result();
 
             // if ($result) {
             //     $response['message'] = "Stock image created";
