406fd26ca96beafc1fa162cb27a137eae42535f2
Bryan <bryanmuliawan@yahoo.co.id>
2021-05-23 13:12:38 +0700

    Update readStockItem

--- readStockItemByUserId.php
+++ readStockItemByUserId.php
@@ -15,18 +15,16 @@ require_once("websiteHeader.html");
 
 <body>
     <?php
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['search'])) {
         session_start();
         $user_id = $_SESSION['user_id'];
 
-        // readStockItemByUserId($user_id);
-        echo $user_id;
+        readStockItemByUserId($user_id);
     }
     ?>
 
-    <form action="readStockItem.php" method="POST" enctype="multipart/form-data">
-        <!-- <label>user_id : </label><input type="text" name="user_id" required><br /> -->
-        <input type="submit" id="submit" name="submit" value="Search">
+    <form action="readStockItemByUserId.php" method="POST" enctype="multipart/form-data">
+        <input type="submit" id="search" name="search" value="Search">
     </form>
 </body>
 
