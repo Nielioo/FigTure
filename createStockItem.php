09aeb80f068f85e0412f3d79ff7e29b4310468c4
Bryan <bryanmuliawan@yahoo.co.id>
2021-05-25 00:17:40 +0700

    Rename image to item

--- createStockItem.php
+++ createStockItem.php
@@ -10,7 +10,7 @@ require_once("websiteHeader.html");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Stock Image</title>
+    <title>Create Stock Item</title>
 </head>
 
 <body>
--- readAllStockItem.php
+++ readAllStockItem.php
@@ -10,7 +10,7 @@ require_once("websiteHeader.html");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Read All Stock Image</title>
+    <title>Read All Stock Item</title>
     <style>
         img {
             max-width: 75;
