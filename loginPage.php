f7a4d1c76b56d78ced1885e13c0cefef82357587
Daniel Aprillio <daprillio@student.ciputra.ac.id>
2021-05-25 13:19:41 +0700

    login style

---
+++ loginPage.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("profile_controller.php");
+require_once("websiteHeader.html");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <img class="bgImage" src="asset/eula2560x1440.jpg">
+    </div>
+
+
+</body>
+
+</html>
