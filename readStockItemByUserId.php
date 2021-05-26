02c3867ea69fa17e320e3e667c4d37e182ec4a0d
Bryan <bryanmuliawan@yahoo.co.id>
2021-05-26 23:54:31 +0700

    Add login checker

--- readStockItemByUserId.php
+++ readStockItemByUserId.php
@@ -18,7 +18,10 @@ require_once("websiteHeader.html");
             object-fit: cover;
         }
 
-        table, th, tr, td {
+        table,
+        th,
+        tr,
+        td {
             border: 1px solid black;
             border-collapse: collapse;
         }
@@ -28,41 +31,48 @@ require_once("websiteHeader.html");
 <body>
     <?php
     session_start();
-    $user_id = $_SESSION['user_id'];
-    $image_list = readStockItemByUserId($user_id);
+
+    if (!empty($_SESSION['user_id'])) {
+        $user_id = $_SESSION['user_id'];
+        $image_list = readStockItemByUserId($user_id);
     ?>
-    <br />
-    <table>
-        <tr>
-            <th>Judul</th>
-            <th>Deskripsi</th>
-            <th>Harga</th>
-            <th>Gambar</th>
-            <th>Tipe</th>
-            <th>Kategori</th>
-            <th>View</th>
-            <th>Edit</th>
-            <th>Delete</th>
-        </tr>
-        <?php
-        foreach ($image_list as $image) {
-            $category_list = implode(",", $image['category']);
-        ?>
+        <br />
+        <table>
             <tr>
-                <td><?= $image['judul'] ?></td>
-                <td><?= $image['deskripsi'] ?></td>
-                <td><?= $image['harga'] ?></td>
-                <td><img src="<?= $image['gambar'] ?>"></td>
-                <td><?= $image['type'] ?></td>
-                <td><?= $category_list ?></td>
-                <td><a href="readStockItemByImageId.php?image_id=<?=$image['image_id']?>">View</a></td>
-                <td><a href="updateStockItem.php?image_id=<?=$image['image_id']?>">Edit</a></td>
-                <td><a href="deleteStockItemByImageId.php?image_id=<?=$image['image_id']?>">Delete</a></td>
+                <th>Judul</th>
+                <th>Deskripsi</th>
+                <th>Harga</th>
+                <th>Gambar</th>
+                <th>Tipe</th>
+                <th>Kategori</th>
+                <th>View</th>
+                <th>Edit</th>
+                <th>Delete</th>
             </tr>
-        <?php
-        }
-        ?>
-    </table>
+            <?php
+            foreach ($image_list as $image) {
+                $category_list = implode(",", $image['category']);
+            ?>
+                <tr>
+                    <td><?= $image['judul'] ?></td>
+                    <td><?= $image['deskripsi'] ?></td>
+                    <td><?= $image['harga'] ?></td>
+                    <td><img src="<?= $image['gambar'] ?>"></td>
+                    <td><?= $image['type'] ?></td>
+                    <td><?= $category_list ?></td>
+                    <td><a href="readStockItemByImageId.php?image_id=<?= $image['image_id'] ?>">View</a></td>
+                    <td><a href="updateStockItem.php?image_id=<?= $image['image_id'] ?>">Edit</a></td>
+                    <td><a href="deleteStockItemByImageId.php?image_id=<?= $image['image_id'] ?>">Delete</a></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+    <?php
+    } else {
+        echo "Please login first";
+    }
+    ?>
 </body>
 
 </html>
