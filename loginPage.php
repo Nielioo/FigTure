ae49210948fbe91ddcc09654aa1d3e52a40d6474
Daniel Aprillio <daprillio@student.ciputra.ac.id>
2021-05-31 07:49:17 +0700

    accelerate login register style

--- loginPage.php
+++ loginPage.php
@@ -13,7 +13,7 @@ require_once("websiteHeader.html");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login_register_style.css">
 </head>
 
 <body>
