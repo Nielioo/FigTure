6d77d38ae0be073ff31c4c9e8d1f0b0db1f8b6ac
Daniel Aprillio <daprillio@student.ciputra.ac.id>
2021-05-31 10:22:06 +0700

    loginPage done :D

--- loginPage.php
+++ loginPage.php
@@ -13,14 +13,55 @@ require_once("websiteHeader.html");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
+    <link rel="stylesheet" href="navigation_bar_style.css">
     <link rel="stylesheet" href="login_register_style.css">
 </head>
 
 <body>
-
     <div class="container">
-        <img class="bgImage" src="asset/eula2560x1440.jpg">
+        <div class="left"></div>
+
+        <div class="right">
+            <div class="header">
+                <h2 class="animation a1">Login</h2>
+                <h4 class="animation a2">Log in to your account using email and password</h4>
+            </div>
+
+            <form class="form" action="loginPage.php" method="post">
+                <input type="email" class="form-field animation a3" placeholder="Email Address">
+                <input type="password" class="form-field animation a4" placeholder="Password">
+                <p class="animation a5"><a href="#">Forgot Password</a></p>
+
+                <input class="form-button animation a6" type="submit" name="login" value="LOGIN">
+            </form>
+
+            <?php
+            if (isset($_POST['login'])) {
+                $conn = connect();
+
+                $email = $_POST["email"];
+                $password = $_POST["password"];
+
+                $query = $conn->prepare("SELECT `user_id`, `email`, `password` FROM `user_profile` WHERE `email` =? AND `password`=?;");
+                $query->bind_param('ss', $email, $password);
+                $query->execute() or die(mysqli_error($conn));
+
+                $result = $query->get_result();
+                $data = $result->fetch_assoc();
+
+                if (!empty($data)) {
+                    session_start();
+                    $_SESSION['user_id'] = $data['user_id'];
+                    echo "[session] user_id = " . $_SESSION['user_id'];
+                } else {
+                    echo "user_id not found";
+                }
+            }
+            ?>
+
+        </div>
     </div>
+
 
 
 </body>
