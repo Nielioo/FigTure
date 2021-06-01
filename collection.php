01ae3e49e79aa1b61753aee2d85f88b866c53513
Bryan <bryanmuliawan@yahoo.co.id>
2021-06-01 14:13:34 +0700

    Update collection

--- collection.php
+++ collection.php
@@ -13,31 +13,30 @@ require_once("websiteHeader.html");
     <title>Collection</title>
     <link rel="stylesheet" href="navigation_bar_style.css">
     <link rel="stylesheet" href="collection_style.css">
-
 </head>
 
 <body>
     <?php
     $image_list = readAllStockItem();
     ?>
-
-    <div class="collection">
-        <?php
-        foreach ($image_list as $image) {
-        ?>
-            <div class="collection_item">
-                <a href="readStockItemByImageId.php?image_id=<?= $image['image_id'] ?>" class="collection_link">
-                    <figure class="collection_figure">
-                        <img src="<?= $image['gambar'] ?>" alt="<?= $image['judul'] ?>" class="collection_image">
-                        <figcaption class="collection_caption"><?= $image['judul'] ?></figcaption>
-                    </figure>
-                </a>
-            </div>
-        <?php
-        }
-        ?>
+    <div class="page">
+        <div class="collection">
+            <?php
+            foreach ($image_list as $image) {
+            ?>
+                <div class="collection_item">
+                    <a href="collectionDetail.php?image_id=<?= $image['image_id'] ?>" class="collection_link">
+                        <figure class="collection_figure">
+                            <img src="<?= $image['gambar'] ?>" alt="<?= $image['judul'] ?>" class="collection_image">
+                            <figcaption class="collection_caption"><?= $image['judul'] ?></figcaption>
+                        </figure>
+                    </a>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
     </div>
-
 </body>
 
 </html>
