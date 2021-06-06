49142854c358ba0412327978b34f2484048ca2bb
Bryan <bryanmuliawan@yahoo.co.id>
2021-06-06 22:17:32 +0700

    Update search by name

--- collectionTitle.php
+++ collectionTitle.php
@@ -29,7 +29,8 @@ if (empty($_SESSION['user_id'])) {
         header("location: collection.php");
     }
 
-    $image_list = readStockItemByTitle($title);
+    // $image_list = readStockItemByTitle($title);
+    $image_list = readAllStockItem();
 
     if (count($image_list) === 0) {
     ?>
@@ -41,16 +42,18 @@ if (empty($_SESSION['user_id'])) {
             <div class="collection">
                 <?php
                 foreach ($image_list as $image) {
+                    if (stripos($image['judul'], $title) !== false) {
                 ?>
-                    <div class="collection_item">
-                        <a href="collectionDetail.php?image_id=<?= $image['image_id'] ?>" class="collection_link">
-                            <figure class="collection_figure">
-                                <img src="<?= $image['gambar'] ?>" alt="<?= $image['judul'] ?>" class="collection_image">
-                                <figcaption class="collection_caption"><?= $image['judul'] ?></figcaption>
-                            </figure>
-                        </a>
-                    </div>
+                        <div class="collection_item">
+                            <a href="collectionDetail.php?image_id=<?= $image['image_id'] ?>" class="collection_link">
+                                <figure class="collection_figure">
+                                    <img src="<?= $image['gambar'] ?>" alt="<?= $image['judul'] ?>" class="collection_image">
+                                    <figcaption class="collection_caption"><?= $image['judul'] ?></figcaption>
+                                </figure>
+                            </a>
+                        </div>
                 <?php
+                    }
                 }
                 ?>
             </div>
